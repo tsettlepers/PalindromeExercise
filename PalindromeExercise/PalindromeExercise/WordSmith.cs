0c314441274bdbd7911536ff78d4fe99d76f9760
Tom Settle <tom.k.settle@gmail.com>
2023-02-27 07:59:13 -0600

    Completed testing

---
+++ PalindromeExercise/PalindromeExercise/WordSmith.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace PalindromeExercise
+{
+    public class WordSmith
+    {
+        public bool IsAPalindrome(string thisWord)
+        {
+            if (string.IsNullOrWhiteSpace(thisWord))
+                return false;
+            else
+            {                
+                int halflength = thisWord.Length / 2;       //Note that we're ignoring the middle letter if there are an odd number of characters.
+                var myCopy = thisWord.ToLower();            //Also note that we're ignoring case.
+
+                for (int i = 0; i < halflength; i++)
+                    if (myCopy[i] != myCopy[myCopy.Length-i-1]) return false;
+                return true;
+            }
+        }
+    }
+}
--- PalindromeExercise/PalindromeExerciseTests/UnitTest1.cs
+++ PalindromeExercise/PalindromeExerciseTests/UnitTest1.cs
@@ -1,3 +1,4 @@
+using PalindromeExercise;
 using System;
 using Xunit;
 
@@ -5,10 +6,23 @@ namespace PalindromeExerciseTests
 {
     public class UnitTest1
     {
-        [Fact]
-        public void Test1()
-        {
+        [Theory]
+        [InlineData("racecar",true)]
+        [InlineData("abCcba",true)]
+        [InlineData("Bob",true)]
+        [InlineData("aa",true)]
+        [InlineData("a",true)]
+        [InlineData("ab",false)]
+        [InlineData("abc",false)]
+        [InlineData("abcdefcba",false)]
+        [InlineData("",false)]
+        [InlineData(null,false)]
 
+        public void Test1(string trialWord, bool presumed)
+        {
+            var ws = new WordSmith();
+            bool actual = ws.IsAPalindrome(trialWord);
+            Assert.Equal(presumed, actual);
         }
     }
 }
